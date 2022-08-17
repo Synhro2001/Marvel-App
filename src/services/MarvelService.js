9637237436e6a6c42479ad723c2fb2b89d05f335
Vadim <vadims.krasovskij01@gmail.com>
2022-08-17 14:41:59 +0300

    Modife code

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,8 +1,10 @@
 
 // otdelnij class kotorij budet na chistom js
 class MarvelService {
-    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=e5160e472c9fe20c74b008126b32bdc6'
+    /*  Shorten url from server  */
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';  
+    _apiKey = 'apikey=e5160e472c9fe20c74b008126b32bdc6';
+    /* Shorten, because value in url similar*/
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -14,14 +16,27 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(
-            `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacters = (id) => {
-        return this.getResource(
-            `${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]); 
+    }
+
+    /* if variable start with _ must to be careful 
+    because if something will changes in function must to be error  */
+    _transformCharacter = (char) => {
+        return {
+            /* Date transform */
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
