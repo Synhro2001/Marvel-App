b1f559b0090099d888067a2bbfaeb2a0e059c221
Vadim <vadims.krasovskij01@gmail.com>
2022-08-17 19:29:08 +0300

    Make good tone for app (Add checks)

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -32,7 +32,7 @@ class MarvelService {
         return {
             /* Date transform */
             name: char.name,
-            description: char.description,
+            description: char.description ? `${char.description.slice(0, 150)} ... ` : `No descriptions`,
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url
